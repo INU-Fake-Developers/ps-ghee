9ea6881f6844b12e15c26dabe1468cd665a15179
GHeeJeon <jhjeon_99@naver.com>
2021-03-11 01:15:09 +0900

    solve 1931

---
+++ problem-solving/main.py
@@ -0,0 +1,12 @@
+score = int(input())
+
+if 90 <= score <= 100:
+    print('A')
+elif 89 <= score <= 90:
+    print('B')
+elif 79 <= score <= 80:
+    print('C')
+elif 69 <= score <= 70:
+    print('D')
+else:
+    print('F')
