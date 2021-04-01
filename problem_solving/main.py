16abccee8d32b34baa1d2f14a531c093ff4e6cdb
GHeeJeon <jhjeon_99@naver.com>
2021-04-01 16:31:50 +0900

    assignment

--- problem_solving/main.py
+++ problem_solving/main.py
@@ -1,16 +1,11 @@
-# This is a sample Python script.
+def add(a, b):
+    # Please write here
+    pass
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+def subtract(a, b):
+    # Please write here
+    pass
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    print(10000000000000+200000)
